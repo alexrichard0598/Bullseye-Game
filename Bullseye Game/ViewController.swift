b6ea8db004c415482d1911a7864b4d6f9f8796f5
Alex Richard <alex.richard0598@pm.me>
2021-01-26 16:47:09 -0500

    idk how to fix this

--- Bullseye Game/ViewController.swift
+++ Bullseye Game/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         selectedVal = Int(targetSlider.value)
-        
+        bullseyeImage.alpha = 0
     }
     
     @IBOutlet weak var targetSlider: UISlider!
