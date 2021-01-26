8db060c921460c1ec875c1647c71a183580f656f
Alex Richard <alex.richard0598@pm.me>
2021-01-26 14:44:57 -0500

    I'm done

--- Bullseye Game/ViewController.swift
+++ Bullseye Game/ViewController.swift
@@ -12,8 +12,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        selectedVal = Int(targetSlider.value)
+        
     }
-
-
+    
+    @IBOutlet weak var targetSlider: UISlider!
+    var selectedVal: Int = 0
+    var numPoints: Int = 0
+    @IBOutlet weak var pointsLable: UILabel!
+    @IBOutlet weak var bullseyeImage: UIImageView!
+    @IBOutlet weak var displayMsg: UILabel!
+    
+    @IBAction func ChangedTarget(_ sender: UISlider) {
+        selectedVal = Int(sender.value)
+        displayMsg.text = String(selectedVal)
+    }
+   
+    @IBAction func Hit(_ sender: Any) {
+        bullseyeImage.alpha = 0
+        
+        let num = Int.random(in: 1..<11)
+        if(num == selectedVal) {
+            bullseyeImage.alpha = 1
+            displayMsg.text = "BULLSEYE!!!"
+            numPoints += 2
+            if(numPoints == 10) {
+                numPoints = 0
+            }
+            pointsLable.text = String(numPoints)
+        } else {
+            displayMsg.text = "SORRY! Better Luck Next Time"
+        }
+    }
+    
+    @IBAction func Reset(_ sender: Any) {
+        numPoints = 0
+        pointsLable.text = String(numPoints)
+        bullseyeImage.alpha = 0
+        targetSlider.value = 5
+        selectedVal = Int(targetSlider.value)
+        displayMsg.text = "Press 'hit It' to Play"
+    }
 }
 
